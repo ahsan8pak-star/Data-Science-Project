1c849c335d92aa06bbb1d5aa7ca90cde2e231b89
ahsan8pak-star <ahsan8pak@gmail.com>
2026-06-15 22:07:28 +0100

    Fogot to call the function itself as well as including try and except commands to prevent the program crashing from a ValueError

--- Python/PrimeNumbers.py
+++ Python/PrimeNumbers.py
@@ -14,5 +14,23 @@ def is_prime(n):
             flag = False
         i = i + 1
     
-    # Output the current value of flag
+    # Output the current value of flag back to the execution zone below
     return flag
+
+
+try: # opens a safe execution zone to process user entries and catch typing errors
+    
+    # Prompts the user to type a number and captures it as a string by default
+    num = input("Enter your number: ")
+    
+    # Converts the text into an integer 
+    n = int(num)
+    
+    # Runs the prime checker function and evaluates the true/false return flag
+    if is_prime(n): 
+        print("The number " + str(n) + " is a prime number!") # confirms prime status safely to the user as a string
+    else:
+        print("The number " + str(n) + " isn't prime.") # updates the user that a clean factor was discovered
+        
+except ValueError: # prevents a crash in the system if the user enters other than integers like floats for example
+    print("Error: Invalid number format. Enter integers only (whole numbers).") # specifies the input rules to guide the operator
