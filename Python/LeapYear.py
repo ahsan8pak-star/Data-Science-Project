0c86dc9dd753c65745f18c2b0a5ff8680fd6d916
ahsan8pak-star <ahsan8pak@gmail.com>
2026-06-17 22:19:45 +0100

    Made a Leap Year function to practice conditional loops

---
+++ Python/LeapYear.py
@@ -0,0 +1,18 @@
+def is_leap(year):
+    
+    leap = False # Assumes it's false since we have no confirmation at the start
+    
+    if year % 4 == 0: # Fundemental rule that every year has to be a multiple of 4 hence every 4 years
+        leap = True
+    
+    else:
+        leap = False
+    
+    return leap
+
+try:
+    year = int(input("Is it a leap year?: ")) # Makes sure that the input is converted to integers since we are using years in that form
+    print(is_leap(year))
+
+except ValueError: # Captures the main error when the user inputs their answer into this function
+    print("Integers only")
