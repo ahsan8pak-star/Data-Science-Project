060e15aa0e667a3661965b8f505f78bb8204bde3
ahsan8pak-star <ahsan8pak@gmail.com>
2026-06-18 18:26:16 +0100

    Made Area of Circle function to practice try and ecept error catching and handling as well as user inputs as a whole

---
+++ Python/AreaOfCircle.py
@@ -0,0 +1,21 @@
+def area_of_circle(radius):
+
+    pi = 3.14
+    area = pi * radius * radius
+    
+    return area
+
+try: 
+    radius = input("Enter Radius: ")
+
+    while radius > 0:
+        print(area_of_circle(radius))
+
+except ValueError:
+    print("Numbers only")
+
+except TypeError:
+    print("Positive Numbers only. Meaning greater than 0.")
+
+except KeyboardInterrupt:
+    print("Unusual Crash Detected.")
