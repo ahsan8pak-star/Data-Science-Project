e20792dee7dfc9b1234b0b50c2097bf68158a735
ahsan8pak-star <ahsan8pak@gmail.com>
2026-06-22 06:19:26 +0100

    Practiced User inputs with the framework of functions to nuderstand how each variable is assigned and stored respectively in their own files

---
+++ Python/Area.py
@@ -0,0 +1,13 @@
+def area (x, y):
+    return x * y
+
+try:
+    x = float(input("Enter Length (cm): "))
+    y = float(input("Enter Width (cm): "))
+
+    result = area(x, y)
+
+    print(f"Area: {round(result, 2)} cm^2")
+
+except ValueError:
+    print("Numbers only")
---
+++ Python/Volume.py
@@ -0,0 +1,14 @@
+def volume (x, y, z):
+    return x * y * z
+
+try:
+    x = float(input("Enter Length (cm): "))
+    y = float(input("Enter Width (cm): "))
+    z = float(input("Enter Depth (cm): "))
+
+    result = volume(x, y, z)
+
+    print(f"Volume: {round(result, 2)} cm^3")
+
+except ValueError:
+    print("Numbers only")
