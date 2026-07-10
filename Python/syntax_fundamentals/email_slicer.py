16fefa89a136dda223114d0a8120bc3a789a98ff
ahsan8pak-star <ahsan8pak@gmail.com>
2026-07-10 20:17:16 +0100

    file(email): - included a validation system checker in the form of moderation operators - used [ while true ] to allow the program to repeat if any errors occur - comments to prevent any confusion in the code - try and except error catching to reduce the likelihood of crashes occuring - used .strip() and other methods to reduce IndexErrors and others alike

--- Python/syntax_fundamentals/email_slicer.py
+++ Python/syntax_fundamentals/email_slicer.py
@@ -1,13 +1,32 @@
-email = input("Enter your email: ")
+while True: # While the program is running
+    try: # main code
+        email = input("Enter your email (or 'q' to quit): ").strip() # prevents accidental <ENTER> imputs
 
-index = email.index("@")
+        if email[0].lower() == 'q':
+            print("Shutting down...")
+            break
 
-username = email[: index] # username = email[: email.index("@")]
+        else: # continues the main program
+            index = email.index("@")
 
-domain = email[index + 1 :] # [index:] -> "@gmail.com" (@ included), [index + 1 :] -> "email.com" (@ excluded)
-# domain = email [email.index("@") + 1 :]
+            username = email[: index] # username = email[: email.index("@")]
 
-print(f"Email: {email}")
-print(f"Username: {username}")
-print(f"Email Domain: {domain}")
+            domain = email[index + 1 :] # [index:] -> "@gmail.com" (@ included), [index + 1 :] -> "email.com" (@ excluded)
+            # domain = email [email.index("@") + 1 :]
+
+            if "@" in email and "." in email: # "@" and "." must be included in 'email'
+                print(f"Email: {email}")
+                print(f"Username: {username}")
+                print(f"Email Domain: {domain}")
+                break # Stops the program running i.e. it's done its task
+
+            else:
+                print("Invalid email. Try again.")
+
+    except ValueError: # Allowing in the form of strings i.e. prevents unexpected crashes
+        print("Invalid input. Avoid Typo Errors.")
+
+    except KeyboardInterrupt:
+        print("We apologise for any inconvenience.")
+        print("Have a great day!")
 
