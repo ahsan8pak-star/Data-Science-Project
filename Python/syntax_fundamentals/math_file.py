4f298a1c9f6192987934090bb528337271389886
ahsan8pak-star <ahsan8pak@gmail.com>
2026-07-11 22:37:52 +0100

    file(math): - included math module to create a mini version of it to test its functionality of importing created module - created math file to test the functionality of the imported created module math_module - adhered to 2 lines underneath the last line for ease of readability and any additional lines of code needed for these programs

---
+++ Python/syntax_fundamentals/math_file.py
@@ -0,0 +1,17 @@
+import math_module
+
+pi_result = math_module.pi
+print(pi_result)
+
+square_result = math_module.square(2)
+print(square_result)
+
+cube_result = math_module.cube(3)
+print(cube_result)
+
+circumference_result = math_module.circumference(4)
+print(circumference_result)
+
+area_result = math_module.area(5)
+print(area_result)
+
