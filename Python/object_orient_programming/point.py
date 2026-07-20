71222eb58685141ed8415e3023141594e644d7de
ahsan8pak-star <ahsan8pak@gmail.com>
2026-07-20 21:50:18 +0100

    file(point): another class file example to understand the fundamentals of classes and methods in oop

---
+++ Python/object_orient_programming/point.py
@@ -0,0 +1,29 @@
+class Point(): # Pascal Naming Convention - > Initialise every word in capital letters
+
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+
+point1 = Point() # variable = class
+point1.draw() # "draw"
+point1.move() # "move"
+
+# Another example of variable assignment
+
+point1.x = 1
+point1.y = 2
+
+print(point1.x)
+print(point1.y)
+
+point2 = Point()
+
+point2.x = 3
+point2.y = 4
+
+print(point2.x)
+print(point2.y)
+
