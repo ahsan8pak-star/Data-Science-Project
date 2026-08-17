3dc8ffaddcd55d3ab3ff1992035d8ac21f8917b8
ahsan8pak-star <ahsan8pak@gmail.com>
2026-08-17 14:06:44 +0100

    chore: update the structure of the comments made on lamda

--- Python/functional_programming/fundamental_topics/lamda.py
+++ Python/functional_programming/fundamental_topics/lamda.py
@@ -1,7 +1,8 @@
 # Lambda function = A small anonymous function for a one time use ('throw away' function)
 # They take any number of arguments, but have only 1 expression
 # Helps keep the namespace clean and is useful with higher-order functions
-# 'sort()', 'map()', 'filter()', 'reduce()' -> example methods of functional programming
+# -> 'sort()', 'map()', 'filter()', 'reduce()' 
+# -> example methods of functional programming
 # lambda parameters: expression
 
 double = lambda x: x * 2
