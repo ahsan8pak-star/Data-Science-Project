68390664bd22b4df906138071651c27cf2cde512
ahsan8pak-star <ahsan8pak@gmail.com>
2026-08-19 18:45:59 +0100

    file(exceptions): include another exception handling example to show a mathematical application

--- Python/imperative_programming/fundamental_topics/exceptions.py
+++ Python/imperative_programming/fundamental_topics/exceptions.py
@@ -8,3 +8,29 @@ try: # Used for the actual executable code to be run underneath
 except ValueError: # Very common example
     print("Enter the age in integers (whole numbers).")
 
+""" Another Example """
+
+# Exception = An event that interrupts the flow of a program
+# (e.g. ZeroDivisionError, TypeError, ValueError)
+# Main methods for exception handling / error catching:
+# 1.try, 2.except, 3.finally
+
+try:
+    number = int(input("Enter an integer for the reciprocal as a decimal: "))
+    print(1 / number)
+
+except ZeroDivisionError:
+    print("Error: Undefined. Zero (0) can't be as a divider.")
+
+except ValueError:
+    print("Enter Valid Numerical Values")
+
+except KeyboardInterrupt:
+    print("Unexpected Crash.\nImmense Apologies.")
+
+except Exception as e:
+    print(f"Unexpected Error: {e}")
+
+finally:
+    print("Proceed Data Cleanup.")
+
