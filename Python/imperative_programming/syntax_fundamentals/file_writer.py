5d3dafaf2003c34567644ba7201e7c08b1f2d7f6
ahsan8pak-star <ahsan8pak@gmail.com>
2026-08-23 22:47:14 +0100

    chrore: rename txt file 'A.I.M' into a.i.m as a result to remove the previous A.I.M.txt file which is placed in the wrong directory

--- Python/imperative_programming/syntax_fundamentals/file_writer.py
+++ Python/imperative_programming/syntax_fundamentals/file_writer.py
@@ -4,7 +4,7 @@ def greet(name):
 
 message = greet("A.I.M")
 
-file = open("AIM.txt", "w") # Opens a file in write mode.
+file = open("aim.txt", "w") # Opens a file in write mode.
 file.write(message) # Writes the greeting message to the file.
 file.close() # Closes the file.
 
