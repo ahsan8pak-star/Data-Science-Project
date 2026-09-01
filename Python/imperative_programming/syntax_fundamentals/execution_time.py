0a95f6f2ee00372d642acafa97279a0eb90f3bbb
ahsan8pak-star <ahsan8pak@gmail.com>
2026-09-01 20:30:14 +0100

    file(execution time): create python file for using import time module through perf.counter()

---
+++ Python/imperative_programming/syntax_fundamentals/execution_time.py
@@ -0,0 +1,12 @@
+import time
+
+start_time = time.perf_counter()
+
+# TODO: Insert Code via importing modules
+
+end_time = time.perf_counter()
+
+executed_time = end_time - start_time
+
+print(f"Execution time: {executed_time:.1f} seconds")
+
