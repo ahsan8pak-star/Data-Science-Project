c23d0e0ec4e2701f98f7ecb21363d2399372d1a3
ahsan8pak-star <ahsan8pak@gmail.com>
2026-09-09 08:27:27 +0100

    feat(generator): include generator expressions as a specific type of generator i.e. a brach of it as similar to list comprehension

--- Python/imperative_programming/fundamental_topics/generator.py
+++ Python/imperative_programming/fundamental_topics/generator.py
@@ -1,11 +1,18 @@
-# Generator = Function that behaves like an iterator (it can be used in a for loop)
-# Pauses a function, returns a value, then resumes
-# Uses 'yield' instead or 'return'
-# Iterate without loading everything into memory (e.g. reading large files)
-# return is like a Pouring bucket = Always empties the function and returns a value
-# yield is like a Drip faucet = Can be paused and resumed
+"""
+Generator = Function that behaves like an iterator (it can be used in a for loop)
 
-# EXAMPLE 1
+Pauses a function, returns a value, then resumes
+Uses yield instead of return
+
+Iterate without loading everything into memory (e.g. reading large files)
+
+return -> Pouring bucket = Always empties the function and returns a value
+yield -> Drip faucet = Can be paused and resumed
+"""
+
+# -----------------
+# --- EXAMPLE 1 ---
+# ----------------- 
 
 import time
 
@@ -50,8 +57,9 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\nGenerator process interrupted by user.")
 
-
-# EXAMPLE 2
+# -----------------
+# --- EXAMPLE 2 ---
+# ----------------- 
 
 def read_file(file_path):
     with open(file_path, "r", encoding = "utf-8") as file:
@@ -67,4 +75,52 @@ for line in read_file(file_directory):
     print(line)
 
 # Note: Ensure activity_log.txt exists at path before iterating
+
+
+"""
+Generator Expression = Similar to a list comprehension but uses () instead of []
+
+Creates a generator (iterator) that yields values one at a time
+No need to define a function or use yield
+Less flexible than a gen func and not reusable
+
+gen object = (expression for value in iterable if condition)
+"""
+
+# -----------------
+# --- EXAMPLE 1 ---
+# ----------------- 
+
+number = int(input("Enter a number to count up to: "))
+
+counter = (count for count in range(1, number + 1))
+
+for n in counter:
+   print(n)
+   time.sleep(1) # Check Comment on Line 20
+
+# -----------------
+# --- EXAMPLE 2 ---
+# ----------------- 
+
+file_path = r"C:\Users\A.I.M\C.S\Data-Science-Project\python\imperative_programming\syntax_fundamentals\\"
+file_name = "aim.txt"
+file_directory = file_path + file_name
+
+with open(file_directory) as file:
+   lines = (line.strip() for line in file)
+
+   for line in lines:
+       print(line)
+
+# -----------------
+# --- EXAMPLE 3 ---
+# ----------------- 
+
+number = int(input("Enter a number to square up to: "))
+
+even_squares = (x ** 2 for x in range(1, number + 1) if x % 2 == 0)
+
+for square in even_squares:
+   print(square)
 
